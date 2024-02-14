aaae825ef15587abdc2f7940f587d7ae283a1d24
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-02-14 19:36:51 +0200

    animation of fire, rolling,

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 export class InputHandler {
-    constructor(){
-        this.keys = [];
+    constructor(game){
+      this.game = game;
+       this.keys = [];
         window.addEventListener("keydown", e => {
           console.log(e.key, this.keys);  
           if ((e.key === "ArrowDown" || 
@@ -10,8 +11,7 @@ export class InputHandler {
             e.key === "Enter"
           ) && this.keys.indexOf(e.key) === -1){
             this.keys.push(e.key);
-          }
-          console.log(e.key, this.keys);
+          } else if (e.key === "d") this.game.debug = !this.game.debug;
 
         });
         window.addEventListener("keyup", e => {
@@ -22,7 +22,6 @@ export class InputHandler {
                 e.key === "Enter"){
                 this.keys.splice(this.keys.indexOf(e.key), 1);
             }
-            console.log(e.key, this.keys);
         })
     }
 }
